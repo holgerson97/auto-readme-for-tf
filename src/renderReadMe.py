088afdcf287bbf811a2714e6b5194a8737533679
Nils Carstensen <nilscarstensen@live.com>
2021-05-14 11:24:25 +0200

    Add: return rendered value

--- src/renderReadMe.py
+++ src/renderReadMe.py
@@ -3,5 +3,6 @@ from jinja2 import Environment, FileSystemLoader
 def render(moduleName, vars, outs, contribute):
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('readme.j2')
-    renderdReadMe = template.render(name = moduleName, variables = vars, outputs = outs, contribute = contribute)
-    print(renderdReadMe)
+    renderdReadMe = template.render(name = moduleName, variables = vars, outputs = outs, contribute = contribute) 
+
+    return renderdReadMe
