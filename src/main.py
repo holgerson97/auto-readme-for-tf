53a6c885826801526c2d6ae3180744c43c08b3f3
nilscarstensen@live.com <nilscarstensen@live.com>
2021-05-24 12:49:07 +0200

    Add: Python3 path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
 import argparse
 import subprocess
 import renderReadMe
 import iterateTerraformFiles
-
 
 def main():
 
@@ -13,13 +13,12 @@ def main():
     with open(args.path + '/README.md', 'w') as file:
         file.write(renderReadMe.render(args.name, args.tableofcontents, args.requirements, args.gettingstarted, struc['versions'], struc['variables'], args.variables, struc['outputs'], args.outputs, args.contribute))
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--path', type=str, default='./module',
-                        help='Specify path to your Terraform module directory. Defaults to "./module".')
+    parser.add_argument('--path', type=str, default='../tests',
+                        help='Specify path to your Terraform module directory. Defaults to "../tests".')
 
     parser.add_argument('--name', type=str, default='default_name',
                         help='Specify the name of your Terraform module. Defaults to "default_name".')
